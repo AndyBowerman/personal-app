8c3ede7458ae776c06bc2c48810e1deeda975b40
AndyBowerman <andy_bowerman06@hotmail.com>
2022-11-20 18:26:38 +0000

    made the site's styling responsive

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,9 +3,7 @@ import Layout from '../../components/Layout/Layout';
 import Weather from '../../containers/Weather/Weather';
 import { Link } from 'react-router-dom';
 import './Home.scss';
-
 import toDoList from "../../assets/to-do-list.jpg";
-import traffic from "../../assets/traffic.jpg";
 
 const Home = () => {
   return (
